808bca7e7d7b4837ba909b8d8ab97529d1c5e0e9
Leandra Fernânda <106930172+LeandraFernanda@users.noreply.github.com>
2025-08-17 19:08:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove("active");
+    dots[i].classList.remove("active");
+  });
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+  currentSlide = index;
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
+}
+
+// Auto-play
+setInterval(nextSlide, 7000); // muda a cada 7 segundos
